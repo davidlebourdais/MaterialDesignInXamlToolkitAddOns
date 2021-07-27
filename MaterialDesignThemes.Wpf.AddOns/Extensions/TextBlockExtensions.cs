62dbadf72edf5eb32667120c7755428c78cc838a
David Le Bourdais <dlb.devx@gmail.com>
2021-07-27 09:30:32 +0200

    [Dev] Add extension to transform TextBock content into TextElements

---
+++ MaterialDesignThemes.Wpf.AddOns/Extensions/TextBlockExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows.Controls;
+using System.Windows.Documents;
+
+namespace MaterialDesignThemes.Wpf.AddOns.Extensions
+{
+    /// <summary>
+    /// Extensions for <see cref="TextBlock"/> elements.
+    /// </summary>
+    public static class TextBlockExtensions
+    {
+        /// <summary>
+        /// Breaks a <see cref="TextBlock"/> content down into <see cref="TextElement"/> objects.
+        /// </summary>
+        /// <param name="textBlock">Source text block.</param>
+        public static void DecomposeIntoTextElements(this TextBlock textBlock)
+        {
+            var span = new Span();
+            while (textBlock.Inlines.Count > 0)
+            {
+                span.Inlines.Add(textBlock.Inlines.FirstInline);
+            }
+
+            textBlock.Inlines.Add(span);
+        }
+    }
+}
